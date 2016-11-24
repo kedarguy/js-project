6befc7caee7b802a8b6be3f59edb3c9370745f40
Guy Kedar <kedarguy@gmail.com>
2016-11-24 11:24:49 +0200

    fixed sizing problem and added random color

--- basics.js
+++ basics.js
@@ -14,43 +14,50 @@ $('#wraper').append(function(){
 
 	}
 
-	
-
-
-
 });
+
+$('.box').height("33");
+$('.box').width('33');
 
 $('.box').hover(function(){
 
-	$(this).css('background-color','yellow');
+	var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+
+	$(this).css('background-color',hue);
 });
 
 $('#button').click(function(){
 
 $('.box').css('background-color','blue')
 
+var $x=1;
+
 $('#wraper').empty();
 
 	var newsqNum=prompt("Please enter number of Squars per row");
-
 
 sqNum = newsqNum*newsqNum;
 
 	for (var i=1; i<=sqNum;i=i+1) {
  $('#wraper').append('<div class="box"></div>');
 
-	}
-
-	
-
-	$('.box').css('height','l1');
-	
+	}	
 
 $('.box').hover(function(){
 
-	$(this).css('background-color','yellow');
+	var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+
+	$(this).css('background-color',hue);
+
 });
+
+var boxH=560/newsqNum-2;
+var boxW=560/newsqNum-2;
+$('.box').height(boxH);
+$('.box').width(boxW);
+
 });
+
 
 
 
